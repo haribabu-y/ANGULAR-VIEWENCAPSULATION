4bb77caf4aab712318f09db3b6689f6093ff3f80
haribabu-y <haribabuitorizon@gmail.com>
2025-09-15 12:20:55 +0530

    final commit

---
+++ src/app/comp2/comp2.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+
+@Component({
+  selector: 'app-comp2',
+  templateUrl: './comp2.component.html',
+  styleUrls: ['./comp2.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class Comp2Component {
+
+}
